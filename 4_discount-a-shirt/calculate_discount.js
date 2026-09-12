aef3db1dd354130c76cb4ac89777f603cd3bcd98
Chamsidine Bacar <chamsidinebacar@gmail.com>
2026-09-12 15:14:32 +0300

    Add tasks 1-4

---
+++ 4_discount-a-shirt/calculate_discount.js
@@ -0,0 +1,9 @@
+
+
+function CalculateDiscount(){
+
+    const price = parseFloat(document.getElementById('price').value);
+    const discount = parseFloat(document.getElementById('discount').value);
+    const quality = parseFloat
+    
+}
