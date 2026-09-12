d93e844b2a121fcb8ebb63d14dbe50f930d3b736
Chamsidine Bacar <chamsidinebacar@gmail.com>
2026-09-12 15:55:07 +0300

    Add tasks some improvement of the style using the bootstrap

--- 4_discount-a-shirt/calculate_discount.js
+++ 4_discount-a-shirt/calculate_discount.js
@@ -1,9 +1,17 @@
-
-
 function CalculateDiscount(){
 
     const price = parseFloat(document.getElementById('price').value);
     const discount = parseFloat(document.getElementById('discount').value);
-    const quality = parseFloat
-    
+    const quality = parseInt(document.getElementById('quality').value);
+
+    const finalPricePerShirt = price - (price * (discount / 100));
+
+    const totalFinalPrice = finalPricePerShirt * quality;
+
+    const message = `Final price per shirt: ugx${finalPricePerShirt.toFixed(2)} <br>
+                    Total for ${quality} shirt: ugx${totalFinalPrice.toFixed(2)}`;
+
+    // utiliser innerHTML au lieu de textContent pour que le <br> soit interprété
+    document.getElementById('output').innerHTML = message;
+    // document.getElementById('output').textContent = message;
 }
